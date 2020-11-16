e5ef15124aac8853943ddcf2956e725775e79c8b
alejanpa17 <alejandro170999@gmail.com>
2020-11-16 10:34:10 -0500

    script tamanno familia

--- Practica 2/script.sh
+++ Practica 2/script.sh
@@ -19,10 +19,20 @@ echo "Porcentaje de lambda"
 
 #Tamano de la poblacion
 echo "Tamano de la poblacion"
-python3 main.py 50 4 0.05 30 3 150 > solutions/pop_50.txt
-python3 main.py 100 4 0.05 60 3 150 > solutions/pop_100.txt
-python3 main.py 150 4 0.05 90 3 150 > solutions/pop_150.txt
-python3 main.py 200 4 0.05 120 3 150 > solutions/pop_200.txt
-python3 main.py 250 4 0.05 150 3 150 > solutions/pop_250.txt
-python3 main.py 300 4 0.05 180 3 150 > solutions/pop_300.txt
-python3 main.py 500 4 0.05 300 3 150 > solutions/pop_500.txt
+# python3 main.py 50 4 0.05 30 3 150 > solutions/pop_50.txt
+# python3 main.py 100 4 0.05 60 3 150 > solutions/pop_100.txt
+# python3 main.py 150 4 0.05 90 3 150 > solutions/pop_150.txt
+# python3 main.py 200 4 0.05 120 3 150 > solutions/pop_200.txt
+# python3 main.py 250 4 0.05 150 3 150 > solutions/pop_250.txt
+# python3 main.py 300 4 0.05 180 3 150 > solutions/pop_300.txt
+# python3 main.py 500 4 0.05 300 3 150 > solutions/pop_500.txt
+
+#Tamano de la familia
+echo "Tamano de la poblacion"
+python3 main.py 300 4 0.05 180 2 150 > solutions/fam_2.txt
+python3 main.py 300 4 0.05 180 3 150 > solutions/fam_3.txt
+python3 main.py 300 4 0.05 180 4 150 > solutions/fam_4.txt
+python3 main.py 300 4 0.05 180 5 150 > solutions/fam_5.txt
+python3 main.py 300 4 0.05 180 6 150 > solutions/fam_6.txt
+python3 main.py 300 4 0.05 180 7 150 > solutions/fam_7.txt
+python3 main.py 300 4 0.05 180 8 150 > solutions/fam_8.txt
